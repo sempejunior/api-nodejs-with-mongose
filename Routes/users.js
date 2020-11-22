85257a28445122d44dbabc0e6f8b3a391e20894c
carlos.sempe <carlos.sempe@zenvia.com>
2020-11-22 16:43:43 -0300

    Code Refactor

--- Routes/users.js
+++ Routes/users.js
@@ -1,55 +1,64 @@
 const express = require('express');
 const router = express.Router();
 const Users = require('../model/user');
-const bcrypt = require ('bcrypt');
+const bcrypt = require('bcrypt');
 
-router.get('/', (req, res) => {
-    Users.find({}, (err, data) => {
-        if (err) return res.send({ error: 'Erro na consulta de usuários' });
-        return res.send(data);
-    });
-})
+router.get('/', async (req, res) => {
 
-router.post('/', (req, res) => {
-    const { email, password } = req.body;
-  
-    if ( !email || !password) {
-        return res.send({ error: 'Dados insufientes' });
+    try {
+        const users = await Users.find();
+        return res.send(users);
+    } catch (error) {
+        res.send({ error: 'Erro na consulta de usuários' });
     }
-    
-    Users.findOne({ email }, (err, data) => {
-        if (err) return res.send({ error: 'Erro ao buscar o usuário' });
-        if (data) return res.send({ error: 'Usuário já registrado' });
-
-        Users.create(req.body, (err, data) => {
-            if (err) return res.send({ error: 'Erro ao buscar o usuário' });
-            data.password = undefined;
-            return res.send(data);
-        });
-
-    });
 
 });
 
-router.post('/auth', (req,res)=>{
-    const {email, password} = req.body;
 
-    if ( !email || !password) {
+router.post('/', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.send({ error: 'Dados insufientes' });
+    }
+    try {
+        if (await Users.findOne({ email })) return res.send({ error: 'Usuário já registrado' });
+
+        const user = await Users.create(req.body);
+        user.password = undefined;
+        return res.send(user);
+
+
+    } catch (error) {
+        res.send({ error: 'Erro ao buscar o usuário' });
+    }
+});
+
+
+
+router.post('/auth', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
         return res.send({ error: 'Dados insufientes' });
     }
 
-    Users.findOne({ email }, (err, data) => {
-        if (err|| !data) return res.send({ error: 'Erro ao buscar o usuário' });
+    try {
+        //I used .select(+password) to return the value because in the model I put it to not return
+        const user = await Users.findOne({ email }).select('+password');
 
-       bcrypt.compare(password, data.password, (err,same)=>{
+        if (!user) res.send({ error: 'Erro na consulta de usuários' });
 
-        if(!same) return res.send({ error: 'Erro ao autenticar o usuário.' });
-        data.password = undefined;
-        return res.send(data);
-       });
+        const samePassword = await bcrypt.compare(password, user.password);
 
-    }).select('+password');
+        if (!samePassword) return res.send({ error: 'Erro ao autenticar o usuário.' });
 
+        user.password = undefined;
+
+        return res.send(user);
+    } catch (error) {
+        return res.send({ error: 'Erro ao buscar o usuário' });
+    }
 });
 
 module.exports = router;
