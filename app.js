c0b16987e13bc545412b7c7017aa9366e5d487fe
carlos.sempe <carlos.sempe@zenvia.com>
2020-11-22 08:27:49 -0300

    Create First Routes

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 
+const indexRoute = require('./Routes/index');
+const usersRoute = require('./Routes/users');
+
+app.use('/', indexRoute);
+app.use('/users', usersRoute);
 
 app.listen(3000);
 module.exports = app;
